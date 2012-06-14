grnet/snf-image-creator

commit 4e58b51b0e69db16e3747c3a6a59133f5ebbea39

Create a new output subpackage

image_creator/main.py
@@ -37,8 +37,8 @@ from image_creator import __version__ as version
 from image_creator import util
 from image_creator.disk import Disk
 from image_creator.util import get_command, FatalError, MD5
-from image_creator.output import Output, Output_wth_progress, Silent, \
-                                                    Silent_wth_colors, error
+from image_creator.output.cli import SilentOutput, SimpleOutput, \
+                                     OutputWthProgress
 from image_creator.os_type import get_os_class
 from image_creator.kamaki_wrapper import Kamaki
 import sys
@@ -160,9 +160,10 @@ def image_creator():
                                                                 "must be set")
 
     if options.silent:
-        out = Silent_wth_colors() if sys.stdout.isatty() else Silent()
+        out = SilentOutput()
     else:
-        out = Output_wth_progress() if sys.stdout.isatty() else Output()
+        out = OutputWthProgress(True) if sys.stderr.isatty() else \
+                                                            SimpleOutput(False)
 
     title = 'snf-image-creator %s' % version
     out.output(title)

image_creator/output/__init__.py
@@ -31,53 +31,51 @@
 # interpreted as representing official policies, either expressed
 # or implied, of GRNET S.A.
 
-import sys
-import pbs
-import hashlib
 
+class Output(object):
+    def error(self, msg, new_line=True):
+        pass
 
-class FatalError(Exception):
-    pass
+    def warn(self, msg, new_line=True):
+        pass
 
+    def success(self, msg, new_line=True):
+        pass
 
-def get_command(command):
-    def find_sbin_command(command, exception):
-        search_paths = ['/usr/local/sbin', '/usr/sbin', '/sbin']
-        for fullpath in map(lambda x: "%s/%s" % (x, command), search_paths):
-            if os.path.exists(fullpath) and os.access(fullpath, os.X_OK):
-                return pbs.Command(fullpath)
-        raise exception
+    def output(self, msg='', new_line=True):
+        pass
 
-    try:
-        return pbs.__getattr__(command)
-    except pbs.CommadNotFount as e:
-        return find_sbin_command(command, e)
+    def _get_progress(self):
+        progress = self._Progress
+        progress.output = self
+        return progress
 
+    Progress = property(_get_progress)
 
-class MD5:
-    def __init__(self, output):
-        self.out = output
+    class _Progress(object):
+        def __init__(self, size, title, bar_type='default'):
+            self.size = size
+            self.output.output("%s..." % title, False)
 
-    def compute(self, filename, size):
+        def goto(self, dest):
+            pass
 
-        MB = 2 ** 20
-        BLOCKSIZE = 4 * MB  # 4MB
+        def next(self):
+            pass
 
-        prog_size = ((size + MB - 1) // MB)  # in MB
-        progressbar = self.out.Progress(prog_size, "Calculating md5sum", 'mb')
-        md5 = hashlib.md5()
-        with open(filename, "r") as src:
-            left = size
-            while left > 0:
-                length = min(left, BLOCKSIZE)
-                data = src.read(length)
-                md5.update(data)
-                left -= length
-                progressbar.goto((size - left) // MB)
+        def success(self, result):
+            self.output.success(result)
 
-        checksum = md5.hexdigest()
-        progressbar.success(checksum)
+    def progress_generator(self, message):
+        def generator(n):
+            progressbar = self.Progress(n, message)
 
-        return checksum
+            for _ in range(n):
+                yield
+                progressbar.next()
+
+            progressbar.success('done')
+            yield
+        return generator
 
 # vim: set sta sts=4 shiftwidth=4 sw=4 et ai :

image_creator/kamaki_wrapper.py
@@ -37,10 +37,8 @@ from kamaki.config import Config
 from kamaki.clients import ClientError
 from kamaki.clients.image import ImageClient
 from kamaki.clients.pithos import PithosClient
-from progress.bar import Bar
 
 from image_creator.util import FatalError
-from image_creator.output import output, warn
 
 CONTAINER = "images"
 

image_creator/output/cli.py
@@ -31,105 +31,55 @@
 # interpreted as representing official policies, either expressed
 # or implied, of GRNET S.A.
 
+from image_creator.output import Output
+
 import sys
-from progress.bar import Bar
 from colors import red, green, yellow
+from progress.bar import Bar
 
 
-def error(msg, new_line=True, color=True):
+def output(msg='', new_line=True, decorate=lambda x: x):
     nl = "\n" if new_line else ' '
-    if color:
-        sys.stderr.write(red('Error: %s' % msg) + nl)
-    else:
-        sys.stderr.write('Error: %s' % msg + nl)
+    sys.stderr.write(decorate(msg) + nl)
 
 
-def warn(msg, new_line=True, color=True):
-    nl = "\n" if new_line else ' '
-    if color:
-        sys.stderr.write(yellow("Warning: %s" % msg) + nl)
-    else:
-        sys.stderr.write("Warning: %s" % msg + nl)
+def error(msg, new_line=True, colored=True):
+    color = red if colored else lambda x: x
+    output("Error: %s" % msg, new_line, color)
 
 
-def success(msg, new_line=True, color=True):
-    nl = "\n" if new_line else ' '
-    if color:
-        sys.stdout.write(green(msg) + nl)
-    else:
-        sys.stdout.write(msg + nl)
-    if not nl:
-        sys.stdout.flush()
+def warn(msg, new_line=True, colored=True):
+    color = yellow if colored else lambda x: x
+    output("Warning: %s" % msg, new_line, color)
 
 
-def output(msg='', new_line=True):
-    nl = "\n" if new_line else ' '
-    sys.stdout.write(msg + nl)
-    if not nl:
-        sys.stdout.flush()
+def success(msg, new_line=True, colored=True):
+    color = green if colored else lambda x: x
+    output(msg, new_line, color)
 
 
-class Output(object):
+class SilentOutput(Output):
+    pass
+
+
+class SimpleOutput(Output):
+    def __init__(self, colored=True):
+        self.colored = colored
 
     def error(self, msg, new_line=True):
-        error(msg, new_line, False)
+        error(msg, new_line, self.colored)
 
     def warn(self, msg, new_line=True):
-        warn(msg, new_line, False)
+        warn(msg, new_line, self.colored)
 
     def success(self, msg, new_line=True):
-        success(msg, new_line, False)
+        success(msg, new_line, self.colored)
 
     def output(self, msg='', new_line=True):
         output(msg, new_line)
 
-    def _get_progress(self):
-        progress = self._Progress
-        progress.output = self
-        return progress
 
-    Progress = property(_get_progress)
-
-    class _Progress(object):
-        def __init__(self, size, title, bar_type='default'):
-            self.output.output("%s..." % title, False)
-            self.size = size
-
-        def goto(self, dest):
-            pass
-
-        def next(self):
-            pass
-
-        def success(self, result):
-            self.output.success(result)
-
-    def progress_generator(self, message):
-        def generator(n):
-            progressbar = self.Progress(message, 'default')
-            progressbar.max = n
-
-            for _ in range(n):
-                yield
-                progressbar.next()
-
-            progressbar.success('done')
-            yield
-        return generator
-
-
-class Output_wth_colors(Output):
-    def error(self, msg, new_line=True):
-        error(msg, new_line)
-
-    def warn(self, msg, new_line=True):
-        warn(msg, new_line)
-
-    def success(self, msg, new_line=True):
-        success(msg, new_line)
-
-
-class Output_wth_progress(Output_wth_colors):
+class OutputWthProgress(SimpleOutput):
     class _Progress(Bar):
         MESSAGE_LENGTH = 30
 
@@ -142,7 +92,7 @@ class Output_wth_progress(Output_wth_colors):
         }
 
         def __init__(self, size, title, bar_type='default'):
-            super(Output_wth_progress._Progress, self).__init__()
+            super(OutputWthProgress._Progress, self).__init__()
             self.title = title
             self.fill = '#'
             self.bar_prefix = ' ['
@@ -158,20 +108,5 @@ class Output_wth_progress(Output_wth_colors):
             self.output.output("\r%s...\033[K" % self.title, False)
             self.output.success(result)
 
-
-class Silent(Output):
-    def warn(self, msg, new_line=True):
-        pass
-
-    def success(self, msg, new_line=True):
-        pass
-
-    def output(self, msg='', new_line=True):
-        pass
-
-
-class Silent_wth_colors(Silent):
-    def error(self, msg, new_line=True):
-        error(msg, new_line)
 
 # vim: set sta sts=4 shiftwidth=4 sw=4 et ai :

commit bb4db5a831cfe6e97de95a9873163d69372deb3e

Fix a bug introduced in 4e58b51b
The error function main.py used for outputing errors has been removed
and the code was not updated to use the error method of SimpleOutput
output class.
Also did a minor cleanup: git_os_class function was renamed to os_cls

image_creator/main.py
@@ -39,7 +39,7 @@ from image_creator.disk import Disk
 from image_creator.util import get_command, FatalError, MD5
 from image_creator.output.cli import SilentOutput, SimpleOutput, \
                                      OutputWthProgress
-from image_creator.os_type import get_os_class
+from image_creator.os_type import os_cls
 from image_creator.kamaki_wrapper import Kamaki
 import sys
 import os
@@ -187,8 +187,8 @@ def image_creator():
         dev = disk.get_device(snapshot)
         dev.mount()
 
-        osclass = get_os_class(dev.distro, dev.ostype)
-        image_os = osclass(dev.root, dev.g, out)
+        cls = os_cls(dev.distro, dev.ostype)
+        image_os = cls(dev.root, dev.g, out)
         out.output()
 
         for sysprep in options.disabled_syspreps:
@@ -283,10 +283,8 @@ def main():
         ret = image_creator()
         sys.exit(ret)
     except FatalError as e:
-        if sys.stdout.isatty():
-            error(e)
-        else:
-            error(e, True, False)
+        colored = sys.stderr.isatty()
+        SimpleOutput(colored).error(e)
         sys.exit(1)
 
 

image_creator/os_type/__init__.py
@@ -37,7 +37,7 @@ import textwrap
 import re
 
 
-def get_os_class(distro, osfamily):
+def os_cls(distro, osfamily):
     module = None
     classname = None
     try: