grnet/snf-image-creator

commit 2e50092b2437913561a37c646e622630d3dd9d12

Make os_type list_sysprep method return 1 list
The method used to return 2 lists of enabled and disabled sysprep
object. Since sysprep objs contail an `enabled' attribute, it is easy
to determine which of those are enabled and which are not. There is
no need to return 2 seperate lists.

image_creator/os_type/__init__.py
@@ -90,10 +90,7 @@ class OSBase(object):
         objs = [getattr(self, name) for name in dir(self) \
             if not name.startswith('_')]
 
-        enabled = [x for x in objs if self._is_sysprep(x) and x.enabled]
-        disabled = [x for x in objs if self._is_sysprep(x) and not x.enabled]
-
-        return enabled, disabled
+        return [x for x in objs if self._is_sysprep(x)]
 
     def _sysprep_change_status(self, name, status):
 
@@ -123,7 +120,9 @@ class OSBase(object):
     def print_syspreps(self):
         """Print enabled and disabled system preperation operations."""
 
-        enabled, disabled = self.list_syspreps()
+        syspreps = self.list_syspreps()
+        enabled = filter(lambda x: x.enabled, syspreps)
+        disabled = filter(lambda x: not x.enabled, syspreps)
 
         wrapper = textwrap.TextWrapper()
         wrapper.subsequent_indent = '\t'

commit f8cccab5464e07d05e2295e38669676b9f2aa543

Fix a bug introduced in 2e50092b2437913561a37c646e
The do_sysprep method was not updated to comply with the changes
made in list_syspreps method

image_creator/os_type/__init__.py
@@ -209,10 +209,12 @@ class OSBase(object):
 
         self.out.output('Preparing system for image creation:')
 
-        tasks, _ = self.list_syspreps()
-        size = len(tasks)
+        tasks = self.list_syspreps()
+        enabled = filter(lambda x: x.enabled, tasks)
+
+        size = len(enabled)
         cnt = 0
-        for task in tasks:
+        for task in enabled:
             cnt += 1
             self.out.output(('(%d/%d)' % (cnt, size)).ljust(7), False)
             task()