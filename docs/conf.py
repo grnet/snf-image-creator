d3f3bfbfbe2a3cc4a39ea3bda97fcdd3887995b1
Nikos Skalkotos <skalkoto@grnet.gr>
2013-03-22 15:06:32 +0200

    Restore html_theme = 'default' in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -94,6 +94,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+html_theme = 'default'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
 html_theme_options = {
        'collapsiblesidebar': 'true',
        'footerbgcolor':    '#55b577',
@@ -115,12 +120,6 @@ html_theme_options = {
        'codebgcolor':      '#eeffcc',
        'codetextcolor':    '#333333'
 }
-
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
