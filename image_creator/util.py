2359b052c543190fb3b432577c517ec7f140cb8d
Nikos Skalkotos <skalkoto@grnet.gr>
2013-06-26 19:09:16 +0300

    Fix an missing func error introduced in e1c0be0296

--- image_creator/util.py
+++ image_creator/util.py
@@ -87,6 +87,26 @@ def free_space(dirname):
     return stat.f_bavail * stat.f_frsize
 
 
+def check_guestfs_version(ghandler, major, minor, release):
+    """Checks if the version of the used libguestfs is smaller, equal or
+    greater than the one specified by the major, minor and release triplet
+
+    Returns:
+        < 0 if the installed version is smaller than the specified one
+        = 0 if they are equal
+        > 0 if the installed one is greater than the specified one
+    """
+
+    ver = ghandler.version()
+
+    for (a, b) in (ver['major'], major), (ver['minor'], minor), \
+            (ver['release'], release):
+        if a != b:
+            return a - b
+
+    return 0
+
+
 class MD5:
     """Represents MD5 computations"""
     def __init__(self, output):
@@ -114,24 +134,5 @@ class MD5:
         progressbar.success(checksum)
 
         return checksum
-
-    def check_guestfs_version(ghandler, major, minor, release):
-        """Checks if the version of the used libguestfs is smaller, equal or
-        greater than the one specified by the major, minor and release triplet
-
-        Returns:
-            < 0 if the installed version is smaller than the specified one
-            = 0 if they are equal
-            > 0 if the installed one is greater than the specified one
-        """
-
-        ver = ghandler.version()
-
-        for (a, b) in (ver['major'], major), (ver['minor'], minor), \
-                (ver['release'], release):
-            if a != b:
-                return a - b
-
-        return 0
 
 # vim: set sta sts=4 shiftwidth=4 sw=4 et ai :
