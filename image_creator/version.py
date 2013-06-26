bda5233f0ad9cb7082e52348ae2d3f521ff2d4b9
Nikos Skalkotos <skalkoto@grnet.gr>
2013-06-26 20:10:55 +0300

    Bump version to 0.4.1next

--- image_creator/version.py
+++ image_creator/version.py
@@ -1,7 +1,7 @@
-__version__ = "0.4next"
-__version_info__ = ['0', '4next']
+__version__ = "0.4.1next"
+__version_info__ = ['0', '4', '1next']
 __version_vcs_info__ = {
     'branch': 'develop',
-    'revid': 'ffc64d7',
-    'revno': 309}
+    'revid': 'ae887b5',
+    'revno': 314}
 __version_user_info__ = "skalkoto@darkstar.admin.grnet.gr"
