5b28403914e7720824330f43e64627a71f658338
Nikos Skalkotos <skalkoto@grnet.gr>
2014-09-10 17:10:31 +0300

    Bump version to 0.17rc1

--- image_creator/version.py
+++ image_creator/version.py
@@ -1,8 +1,8 @@
 
-__version__ = "0.6.2next"
+__version__ = "0.7rc1"
 __version_vcs_info__ = {
-    'branch': 'develop',
-    'revid': '15e2ef7',
-    'revno': 452}
+    'branch': 'release-0.7',
+    'revid': '96d50cb',
+    'revno': 555}
 __version_user_email__ = "skalkoto@grnet.gr"
 __version_user_name__ = "Nikos Skalkotos"
