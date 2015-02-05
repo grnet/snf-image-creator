45fa3bcf67476bb31f6abbdf4d1ed12f4ebaeb83
Nikos Skalkotos <skalkoto@grnet.gr>
2015-02-05 11:38:50 +0200

    Update version.py and ChangeLog for v0.8.1

--- image_creator/version.py
+++ image_creator/version.py
@@ -1,8 +1,8 @@
 
-__version__ = "0.8"
+__version__ = "0.8.1"
 __version_vcs_info__ = {
-    'branch': 'master',
-    'revid': 'ea04ad1',
-    'revno': 654}
+    'branch': 'hotfix-0.8.1',
+    'revid': '27ddd5e',
+    'revno': 657}
 __version_user_email__ = "skalkoto@grnet.gr"
 __version_user_name__ = "Nikos Skalkotos"
