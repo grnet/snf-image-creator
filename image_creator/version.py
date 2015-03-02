77d2b6b13577bbef995b111e0cbb79656e59ef4c
Nikos Skalkotos <skalkoto@grnet.gr>
2015-03-02 14:42:37 +0200

    Update version files and ChangeLog for v0.9

--- image_creator/version.py
+++ image_creator/version.py
@@ -1,8 +1,8 @@
 
-__version__ = "0.9rc2"
+__version__ = "0.9"
 __version_vcs_info__ = {
-    'branch': 'release-0.9',
-    'revid': '57e48a9',
-    'revno': 686}
+    'branch': 'master',
+    'revid': '9a60629',
+    'revno': 689}
 __version_user_email__ = "skalkoto@grnet.gr"
 __version_user_name__ = "Nikos Skalkotos"
