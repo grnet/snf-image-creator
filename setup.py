b666323cc30ad0fc30fd975db0f21a76baba6d34
Nikos Skalkotos <skalkoto@grnet.gr>
2017-06-29 11:05:13 +0300

    Rename yaml dependency to pyyaml
    This is the name the setuptools expect to find.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=['sh', 'ansicolors', 'progress>=1.0.2', 'kamaki>=0.9',
-                      'argparse', 'yaml'],
+                      'argparse', 'pyyaml'],
     # Unresolvable dependencies:
     #   pysendfile|py-sendfile, hivex, guestfs, parted, rsync,
     entry_points={
